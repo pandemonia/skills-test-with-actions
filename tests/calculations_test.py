23c20f4fb31d5d510f6e78cbdff69c6f1982c7f7
Nalin Ilango <2588635+pandemonia@users.noreply.github.com>
2026-06-02 06:30:01 +0000

    Update tests

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -33,6 +33,24 @@ def test_area_of_circle_zero_radius():
     # Assert
     assert result == 0
 
+def test_area_of_circle_negative_radius():
+    """Test with a negative radius to raise ValueError."""
+    # Arrange
+    radius = -1
+
+    # Act & Assert
+    with pytest.raises(ValueError):
+      area_of_circle(radius)
+
+def test_get_nth_fibonacci_negative():
+    """Test with n=-1."""
+    # Arrange
+    n = -1
+
+    # Act & Assert
+    with pytest.raises(ValueError):
+      get_nth_fibonacci(n)
+
 
 def test_get_nth_fibonacci_zero():
     """Test with n=0."""
@@ -67,4 +85,4 @@ def test_get_nth_fibonacci_ten():
     result = get_nth_fibonacci(n)
 
     # Assert
-    assert result == 89
+    assert result == 55
